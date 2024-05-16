0cc18e7a033649ba285fae55c04dc77e251ef9ea
Gangavane Ajit <90243943+Ajitg2001@users.noreply.github.com>
2024-05-16 23:32:17 +0530

    Delete TELECALLER directory

--- TELECALLER/ClientDetails.js
+++
@@ -1,9 +0,0 @@
-export default function ClientDetails({clientName,clientAddress})
-{
-    return(
-        <section className="mt-5">
-                    <h2 className="text-xl uppercase">{clientName}</h2>
-                    <p>{clientAddress}</p>
-        </section>
-    )
-}
